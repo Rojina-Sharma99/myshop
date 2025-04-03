064105a5d0cced2b23cafb3b7c50f0d9b1a1be0b
Rojina-Sharma99 <sharmarojina99@gmail.com>
2025-04-03 20:27:22 +1030

    Added home page , with Nav, header, footer
    Home screen with nav bar, header footer has been added . Also added Product screen with product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import {Outlet} from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
 
 const App = () => {
   return (
-    <h1>Welcome to myshop</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3"> 
+        <Container>
+         <Outlet/>
+        </Container>
+      </main>
+        <Footer/>
 
-export default App
+      
+    </>
+  );
+};
+
+export default App;
