50365d865983dbe29232209c3a68173607c76c05
Rojina-Sharma99 <sharmarojina99@gmail.com>
2025-05-31 15:30:38 +0930

    started with express,
    Checking first express app listening to port 3000

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Hello Roji welcome")
+import express from "express";
+import products from "./data/products.js";
+
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
