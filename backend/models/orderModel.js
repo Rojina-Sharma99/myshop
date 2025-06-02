2ea8bb26dd36a53d58e2db2a25566849980ada10
Rojina-Sharma99 <sharmarojina99@gmail.com>
2025-06-02 11:14:37 +0930

    Create orderModel.js

---
+++ backend/models/orderModel.js
@@ -0,0 +1,90 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+
+    //an array of items
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Model.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+
+    paymentmethod: {
+      type: String,
+      required: true,
+    },
+
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    //in javascxript we dont have float or double so type will be the number
+
+    itemsprice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalprice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isdelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
